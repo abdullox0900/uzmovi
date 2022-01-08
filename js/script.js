544e3039636bfe173af356c79e6574ee47da2cc3
abdullox0900 <abdullox0900@gmail.com>
2022-01-09 02:51:08 +0500

    add css style

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let elMovieList = document.querySelector(".movie__list");
+let elLink = "https://www.youtube.com/watch?v=";
+
+for (let movie of movies) {
+
+     // CREATE
+     let movirItem = document.createElement("li");
+}
